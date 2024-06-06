27ca489b6ca41edb33a03eb8bb58e294354a4255
Webslingr <jsunnyassy@yahoo.com>
2024-06-06 00:53:22 -0300

    First swift project

--- Emoji Picker/ContentView.swift
+++ Emoji Picker/ContentView.swift
@@ -7,15 +7,30 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 🔥, 🎉, 🥳, 🍕
+}
+
 struct ContentView: View {
+    
+    @State var selection: Emoji = .🎉
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Select an emoji")
+            .padding(50)
         }
-        .padding()
     }
 }
 
