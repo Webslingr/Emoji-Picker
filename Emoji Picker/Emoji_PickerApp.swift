0ea22dd2271289dd902207e2221216f08649fe11
Webslingr <jsunnyassy@yahoo.com>
2024-06-05 21:51:51 -0300

    Initial Commit

---
+++ Emoji Picker/Emoji_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_PickerApp.swift
+//  Emoji Picker
+//
+//  Created by Jason Sunnyassy on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
